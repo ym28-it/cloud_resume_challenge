c81a673a307bd92dc3e629bdd2383e5aacd311d8
ym28-it <120317084+ym28-it@users.noreply.github.com>
2026-05-03 14:38:43 +0900

    crc: lambda func complete: connect dynamodb

--- lambda/visitor_couter/handler.py
+++ lambda/visitor_couter/handler.py
@@ -1,21 +1,43 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 
+
+# --- Initialize ---
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('crc-prac-db')
+COUNTER_ID = 'counter'
 
 def lambda_handler(event, context):
-    new_visitor = increase_visitor()
-    response = {
-        "statusCode": 200,
-        "headers": {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        "body": str(new_visitor)
-    }
-    return response
+    try:
+        new_visitor = increase_visit_count()
+        response = {
+            "statusCode": 200,
+            "headers": {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            "body": str(new_visitor)
+        }
+        return response
+    except ClientError as e:
+        error_code = e.response['Error']['Code']
+        print(f'DynamoDB error: {error_code}')
+        raise
 
+def increase_visit_count():
+    response = table.update_item(
+        Key={'id': COUNTER_ID},
+        UpdateExpression='ADD #val :increment',
+        ExpressionAttributeNames = {'#val': 'value'},
+        ExpressionAttributeValues={':increment': 1},
+        ReturnValues='ALL_NEW'
+    )
+    return response['Attributes']['value']
 
-visitor = 0
-def increase_visitor():
-    global visitor
-    visitor += 1
-    return visitor
+# visitor = 0
+# def increase_visitor():
+#     global visitor
+#     visitor += 1
+#     return visitor
